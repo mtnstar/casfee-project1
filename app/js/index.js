321d173580785fa3d157acee8cf42a79f3edaa3b
Mountain Star <github@mtnstar.net>
2022-05-18 20:25:20 +0200

    Consume rails api :)

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,20 @@
+function fetchTasks() {
+   const myRequest = new Request('http://localhost:3000/tasks.json');
+   fetch(myRequest)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${ response.status }`);
+            }
+            return response.json();
+        })
+        .then((json) => {
+            const tasks = document.querySelector('#tasks');
+            tasks.insertAdjacentHTML('beforebegin', json[0].title);
+        });
+}
+
 const btn1 = document.querySelector('#btn-name')
 
-btn1.addEventListener('click',
-    ()=> { console.log('asdfasdf'); });
+btn1.addEventListener('click', function() {
+    fetchTasks();
+});
